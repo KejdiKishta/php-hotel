01b54399a79cbd7c51b4265a07e12ffe8f347710
Kejdi Kishta <155896977+KejdiKishta@users.noreply.github.com>
2024-05-21 16:07:56 +0200

    add: table style

--- index.php
+++ index.php
@@ -66,11 +66,45 @@ Se non viene specificato nessun filtro, visualizzare come in precedenza tutti gl
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body class="bg-primary">
-    <h1 class="text-center fw-bold text-white my-4">PHP Hotels</h1>
-    <?php foreach ($hotels as $cur_hotel) { ?>
-        <?php foreach ($cur_hotel as $key => $value) { ?>
-            <?php echo $value . "<br>"?>
-        <?php } ?>
-    <?php } ?>
+    <div class="container">
+        <h1 class="text-center fw-bold text-white my-4">PHP Hotels</h1>
+
+        <table class="table table-dark table-striped">
+            <thead>
+                <?php foreach ($hotels[0] as $key => $value) { ?>
+                    <?php echo "<th scope='col'>" ?>
+                    <?php echo $key ?>
+                    <?php echo "</th>" ?>
+                <?php } ?>
+            </thead>
+            <tbody>
+        
+                <?php foreach ($hotels as $cur_hotel) { ?>
+                    <?php echo "<tr>" ?>
+                    <?php foreach ($cur_hotel as $key => $value) { ?>
+                        <?php echo "<td>" ?>
+                        <?php
+                            if ($key === "parking") {
+                                if ($value === true) {
+                                    $value = "<span class='text-success fw-bold'>&check;</span>";
+                                } elseif ($value === false) {
+                                    $value = "<span class='text-danger fw-bold'>&cross;</span>";
+                                }
+                            } elseif ($key === "vote") {
+                                $value = "{$value} &#9733;";
+                            } elseif ($key === "distance_to_center") {
+                                $value = "{$value} km";
+                            }
+                            
+                            echo $value;
+                        ?>
+                        <?php echo "</td>" ?>
+                    <?php } ?>
+                    <?php echo "</tr>" ?>
+                <?php } ?>
+            
+            </tbody>
+        </table>
+    </div>
 </body>
 </html>
